dd9296a12a8523292b23318b998f2a7b005c5870
charanjeet77 <sikkaittech@gmail.com>
2024-11-14 06:24:57 +0530

    mywebsite first work

---
+++ mailing/mailingvariables.php
@@ -0,0 +1,7 @@
+<?php
+    $mail_host = "smtp.gmail.com";
+    $mail_port = "587";
+    $mail_sender_email = "sikkaittech@gmail.com"; //sender// your email address
+    $mail_sender_password = "cdaw smax hxyt tqfp"; //sender// your gmail  app pasword
+    $mail_sender_name = "Website Form";
+?>
